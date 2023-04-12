2bea972b2823dbe27fc91b85ecab66522466348f
Fanuel Ramos <fanuelramos111@gmail.com>
2023-04-12 02:16:37 +0100

    test: ensure StudentYupValidator throws if an invalid email is given

--- src/modules/student/entity/student-entity.spec.ts
+++ src/modules/student/entity/student-entity.spec.ts
@@ -8,4 +8,12 @@ describe('StudentEntity test', () => {
       email: ''
     })).toThrow()
   })
+
+  test('Should throw if an invalid email is given', () => {
+    expect(() => new Student({
+      name: 'any_name',
+      phone: '+244900000000',
+      email: 'any_email'
+    })).toThrow()
+  })
 })
