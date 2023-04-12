f9078080b5cf13fecf65bc7229cc85ffbbf2eeb0
Fanuel Ramos <fanuelramos111@gmail.com>
2023-04-12 02:39:14 +0100

    feat: add find method on StudentGateway interface

--- src/modules/student/gateway/student-gateway.ts
+++ src/modules/student/gateway/student-gateway.ts
@@ -1,5 +1,7 @@
+import { FilterQuery } from 'mongoose'
 import Student from '../entity/student-entity'
 
 export default interface StudentGateway {
   add(student: Student): Promise<void>
+  find(filter: FilterQuery<unknown>): Promise<Student | null>
 }
