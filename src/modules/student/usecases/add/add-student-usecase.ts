009597938a79b9e3b1422a1b4cd946d24331c20e
Fanuel Ramos <fanuelramos111@gmail.com>
2023-04-12 03:32:44 +0100

    feat: ensure AddStudentUseCase throws if an already existen email is provided

--- src/modules/student/usecases/add/add-student-usecase.ts
+++ src/modules/student/usecases/add/add-student-usecase.ts
@@ -1,4 +1,5 @@
 import UseCaseInterface from '../../../_shared/usecases/usecase-interface'
+import { InvalidParamError } from '../../../_shared/utils/errors'
 import Student from '../../entity/student-entity'
 import StudentGateway from '../../gateway/student-gateway'
 import { AddStudentUseCaseInputDTO, AddStudentUseCaseOutputDTO } from './add-student-usecase-dto'
@@ -7,6 +8,10 @@ export default class AddStudentUseCase implements UseCaseInterface<AddStudentUse
   constructor (private readonly _repository: StudentGateway) {}
 
   async execute (input?: AddStudentUseCaseInputDTO): Promise<AddStudentUseCaseOutputDTO> {
+    const emailAlreadyExists = await this._repository.find({ email: input.email })
+    if (emailAlreadyExists) {
+      throw new InvalidParamError('email')
+    }
     const student = new Student(input)
     await this._repository.add(student)
 
