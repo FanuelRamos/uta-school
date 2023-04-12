9c9b013dbb199228eac5a4b5341b42ed8a1bc918
Fanuel Ramos <fanuelramos111@gmail.com>
2023-04-12 01:37:03 +0100

    feat: add Student entity

---
+++ src/modules/student/entity/student-entity.ts
@@ -0,0 +1,32 @@
+import BaseEntity from '../../_shared/domain/entity/base-entity'
+import Id from '../../_shared/domain/value-object/id-value-object'
+
+type StudentProps = {
+  id?: Id
+  name: string
+  phone: string
+  email: string
+  createdAt?: Date
+  updatedAt?: Date
+}
+
+export default class StudentEntity extends BaseEntity {
+  private _name: string
+  private _phone: string
+  private _email: string
+
+  constructor (props: StudentProps) {
+    super(props.id, props.createdAt, props.updatedAt)
+    this._name = props.name
+    this._phone = props.phone
+    this._email = props.email
+  }
+
+  get name (): string { return this._name }
+  get phone (): string { return this._phone }
+  get email (): string { return this._email }
+
+  set name (value: string) { this._name = value }
+  set phone (value: string) { this._phone = value }
+  set email (value: string) { this._email = value }
+}
