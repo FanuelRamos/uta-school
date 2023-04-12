ce6e8454a6501969f8efe1283f3ee24bf3c4002b
Fanuel Ramos <fanuelramos111@gmail.com>
2023-04-12 02:02:41 +0100

    feat: add StudentYupValidator to validate the properties

---
+++ src/modules/student/validators/student-yup-validator.ts
@@ -0,0 +1,32 @@
+import ValidatorInterface from '../../_shared/validators/validator-interface'
+import Student from '../entity/student-entity'
+import * as yup from 'yup'
+
+export default class StudentYupValidator implements ValidatorInterface<Student> {
+  validate (entity: Student): void {
+    try {
+      yup
+        .object()
+        .shape({
+          name: yup.string().required('Name is required'),
+          phone: yup.string().required('Phone is required'),
+          email: yup.string().email().required('Email is required')
+        })
+        .validateSync(
+          {
+            name: entity.name,
+            phone: entity.phone,
+            email: entity.email
+          },
+          {
+            abortEarly: false
+          }
+        )
+    } catch (errors) {
+      const e = errors as yup.ValidationError
+      e.errors.forEach((error) => {
+        throw new Error(error)
+      })
+    }
+  }
+}
