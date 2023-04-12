7bab8818ebd29ff2844c522e1995566d1afd411e
Fanuel Ramos <fanuelramos111@gmail.com>
2023-04-12 02:23:30 +0100

    test: ensure StudentEntity creates a Student

--- src/modules/student/entity/student-entity.spec.ts
+++ src/modules/student/entity/student-entity.spec.ts
@@ -16,4 +16,17 @@ describe('StudentEntity test', () => {
       email: 'any_email'
     })).toThrow()
   })
+
+  test('Should be able to create a Student', () => {
+    const student = new Student({
+      name: 'any_name',
+      phone: '+244900000000',
+      email: 'any_email@mail.com'
+    })
+    expect(student).toBeTruthy()
+    expect(student.id).toBeTruthy()
+    expect(student.name).toBe('any_name')
+    expect(student.phone).toBe('+244900000000')
+    expect(student.email).toBe('any_email@mail.com')
+  })
 })
