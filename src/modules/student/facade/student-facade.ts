ad7e6eb76545f32145a8f3077d40d0fcd2231977
Fanuel Ramos <fanuelramos111@gmail.com>
2023-04-17 20:57:26 +0100

    feat: add StudentFacade

---
+++ src/modules/student/facade/student-facade.ts
@@ -0,0 +1,26 @@
+import UseCaseInterface from '../../_shared/usecases/usecase-interface'
+import { HttpResponse } from '../../_shared/utils/helpers/http'
+import { AddStudentFacadeInputDTO, FindStudentFacadeInputDTO, StudentFacadeInterface } from './student-facade-interface'
+
+interface UseCaseProps {
+  addStudentUseCase: UseCaseInterface<AddStudentFacadeInputDTO, HttpResponse>
+  findStudentUseCase: UseCaseInterface<FindStudentFacadeInputDTO, HttpResponse>
+}
+
+export default class StudentFacade implements StudentFacadeInterface {
+  private _addStudentUseCase: UseCaseInterface<AddStudentFacadeInputDTO, HttpResponse>
+  private _findStudentUseCase: UseCaseInterface<FindStudentFacadeInputDTO, HttpResponse>
+
+  constructor (props: UseCaseProps) {
+    this._addStudentUseCase = props.addStudentUseCase
+    this._findStudentUseCase = props.findStudentUseCase
+  }
+
+  async add (input: AddStudentFacadeInputDTO): Promise<HttpResponse> {
+    return await this._addStudentUseCase.execute(input)
+  }
+
+  async find (input: FindStudentFacadeInputDTO): Promise<HttpResponse> {
+    return await this._findStudentUseCase.execute(input)
+  }
+}
