ce5aef9bad17fe8995c3561f123708c8ffb4c937
Fanuel Ramos <fanuelramos111@gmail.com>
2023-04-19 11:22:28 +0100

    feat: add mongodb connection and then start the app

---
+++ src/main/server.ts
@@ -0,0 +1,21 @@
+import dotenv from 'dotenv'
+import app from './config/app'
+import mongoose from 'mongoose'
+
+dotenv.config()
+
+const connectDB = async (): Promise<void> => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URI)
+    console.log(`MongoDB Connected: ${conn.connection.host}`)
+  } catch (error) {
+    console.log(error)
+    process.exit(1)
+  }
+}
+
+void connectDB().then(() => {
+  app.listen(process.env.PORT, () => {
+    console.log('Listening for requests...')
+  })
+})
