8f6869b709d65b0311a25c39699ad3ee6ee11789
Fanuel Ramos <fanuelramos111@gmail.com>
2023-04-24 23:06:22 +0100

    feat: add post route for Student module

---
+++ src/main/routes/student-routes.ts
@@ -0,0 +1,7 @@
+import { Router } from 'express'
+import StudentFacadeFactory from '../../modules/student/factories/student-facade-factory'
+import { adaptRoute } from '../adapters/express-route-adapter'
+
+export default (router: Router): void => {
+  router.post('/student', adaptRoute(StudentFacadeFactory, 'add'))
+}
