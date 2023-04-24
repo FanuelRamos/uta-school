b280c2d48803df5a489e89ae258daff9982e8a39
Fanuel Ramos <fanuelramos111@gmail.com>
2023-04-24 23:45:36 +0100

    test: ensure get route of Student module returns 200 on success

--- src/main/routes/student-routes.test.ts
+++ src/main/routes/student-routes.test.ts
@@ -1,22 +1,48 @@
 import mongoose from 'mongoose'
 import request from 'supertest'
 import app from '../config/app'
+import { afterEach } from 'node:test'
 
 let conn = null
 
 describe('Student routes test', () => {
   beforeAll(async () => {
-    conn = await mongoose.connect(process.env.MONGO_URI)
+    conn = await mongoose.connect(process.env.MONGO_URI_TEST)
+  })
+
+  beforeEach(async () => {
+    await mongoose.connection.dropCollection('students')
   })
 
   describe('POST /student', () => {
-    test('Should return 200 on signup', async () => {
+    test('Should return 200 on subscribe', async () => {
       await request(app)
         .post('/api/student')
         .send({
           name: 'any_name',
           phone: '+244 900000000',
           email: 'any_email@mail.com'
+        })
+        .expect(200)
+    })
+  })
+
+  describe('GET /student', () => {
+    test('Should return 200 on find Student', async () => {
+      await request(app)
+        .post('/api/student')
+        .send({
+          name: 'any_name',
+          phone: '+244 900000000',
+          email: 'any_email@mail.com'
+        })
+
+      await request(app)
+        .get('/api/student')
+        .send({
+          filter: {
+            email: 'any_email@mail.com'
+          }
         })
         .expect(200)
     })
